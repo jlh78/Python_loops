ce75f399b2a094fc7969aeb99c6968518d0c7c8a
Jonah Hunter <jlh7836@gmail.com>
2025-04-21 17:01:59 -0400

    multi cond check

--- loops.py
+++ loops.py
@@ -3,3 +3,19 @@ end = int(input("ending number:" ))
 while start < end : 
     start += 1 
     print(f"{start}")
+
+
+
+
+            
+            
+
+        
+
+forbidden_text = "Practical uses for the Necromoicon"
+counter = 0
+while counter < len(forbidden_text):
+    # print out every other letters, but leave out spaces
+    if (counter % 2) == 0 and forbidden_text[counter] != " ":
+        print(forbidden_text[counter])
+    counter = counter + 1
